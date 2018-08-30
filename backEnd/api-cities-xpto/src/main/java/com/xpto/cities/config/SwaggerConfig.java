c8a27b7808110ef38051de7cfe36dbb7fc96b622
Julio Souza <juliojr.souza@gmail.com>
2018-08-30 13:58:49 -0300

    swagger test

---
+++ backEnd/api-cities-xpto/src/main/java/com/xpto/cities/config/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package com.xpto.cities.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+	@Bean
+	public Docket apiCitiesXpto() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.xpto.cities"))
+				.paths(regex("/controller.*"))
+				.build()
+				.apiInfo(metaInfo());
+	}
+
+	private ApiInfo metaInfo() {
+
+		ApiInfo apiInfo = new ApiInfo("Spring Boot Swagger Example API", "Spring Boot Swagger Example API for Youtube",
+				"1.0", "Terms of Service",
+				new Contact("Julio Souza", "https://github.com/juliojr", "juliojr.souza@gmail.com"),
+				"Apache License Version 2.0", "https://www.apache.org/licesen.html");
+
+		return apiInfo;
+	}
+
+}
