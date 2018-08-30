317c07c765d44a711b32783042d1141a81d4c759
Julio Souza <juliojr.souza@gmail.com>
2018-08-30 14:13:01 -0300

    swagger works

--- backEnd/api-cities-xpto/src/main/java/com/xpto/cities/config/SwaggerConfig.java
+++ backEnd/api-cities-xpto/src/main/java/com/xpto/cities/config/SwaggerConfig.java
@@ -2,6 +2,8 @@ package com.xpto.cities.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
@@ -9,18 +11,14 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import static springfox.documentation.builders.PathSelectors.regex;
-
 @EnableSwagger2
 @Configuration
 public class SwaggerConfig {
 	@Bean
 	public Docket apiCitiesXpto() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("com.xpto.cities"))
-				.paths(regex("/controller.*"))
-				.build()
-				.apiInfo(metaInfo());
+				.apis(RequestHandlerSelectors.basePackage("com.xpto.cities.controller")).paths(PathSelectors.any())
+				.build().apiInfo(metaInfo());
 	}
 
 	private ApiInfo metaInfo() {
@@ -32,5 +30,5 @@ public class SwaggerConfig {
 
 		return apiInfo;
 	}
-
+	
 }
